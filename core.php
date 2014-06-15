5add59b4c3f24879aebe6c4a3f1e3814ce13d5fa
BWZ <ben@Bens-MacBook-Air.local>
2014-06-16 09:25:16 +1000

    added random string function

--- core.php
+++ core.php
@@ -4,7 +4,7 @@
 // Start a session and create a userID if one does not already exist
 session_start();
 if (!isset($_SESSION['userid'])) {
-	$_SESSION['userid'] = genKey(40);
+	$_SESSION['userid'] = genRandomString(40);
 }
 $userid = $_SESSION['userid'];
 
@@ -13,6 +13,16 @@ $userid = $_SESSION['userid'];
 $db = mysqli_connect($db_server, $db_username, $db_password, $db_name);
 if (mysqli_connect_errno($db)) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+//function to generate a random string
+function genRandomString($l) {
+	$alfa = "23456789qwertyuiopasdfghjkzxcvbnm";
+    $string = "";
+    for($i = 0; $i < $l; $i++) {
+      	$string .= $alfa[rand(0, strlen($alfa)-1)];
+    }
+    return $string;
 }
 
 ?>
