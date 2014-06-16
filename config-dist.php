36b8ec6a7ae2c54d94fe4752ab74b42b99a85698
BWZ <ben@Bens-MacBook-Air.local>
2014-06-16 10:51:21 +1000

    added config items

--- config-dist.php
+++ config-dist.php
@@ -2,12 +2,21 @@
 
 // RENAME THIS FILE TO config.php
 
-//Details used to connect to the DB
+// Details used to connect to the DB
 $db_server = "localhost";
 $db_username = "root";
 $db_password = "root";
 $db_name = "securedrop";
 
 $config['requireHTTPS'] = false;
+
+// Maximum sile in MB - ensure that this is the same or lower than your php.ini settings for
+// memory_limit
+// post_max_size
+$config['max_file_size'] = 30;
+
+// File expiry time in hours, after this ends the file will be removed
+// by the cron job
+$config['file_expiry'] = 7 * 24;
 
 ?>
