c10012b9b04362e5c7e32733aeb15944368b5ceb
BWZ <ben@delonge.com.au>
2014-08-18 18:31:13 +1000

    improved file download function

--- public/_interfaces/upload.php
+++ public/_interfaces/upload.php
@@ -66,21 +66,6 @@ if (!empty($_FILES)) {
 				$filecrypt = new filecrypt();
 				
 				 $filecrypt->encryptFileChunks($_FILES['file']['tmp_name'], $targetFile, $passphrase);
-				 
-				 //$filecrypt->decryptFileChunks('encrypted.gif', 'decrypted.gif', $key);
-				
-				/* 
-				$iv = substr(md5("\x1B\x3C\x58".$passphrase, true), 0, 8);
-				$key = substr(md5("\x2D\xFC\xD8".$passphrase, true) .
-				md5("\x2D\xFC\xD9".$passphrase, true), 0, 24);
-				$opts = array('iv'=>$iv, 'key'=>$key);
-				 
-				$fp = fopen($targetFile, 'wb');
-				stream_filter_append($fp, 'mcrypt.tripledes', STREAM_FILTER_WRITE, $opts);
-				fwrite($fp, file_get_contents($_FILES['file']['tmp_name']));
-				 
-				fclose($fp);
-				*/
 			
 			} else {
 				 
